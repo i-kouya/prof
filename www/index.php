fc0d06622fa0caeeb8c45c2238f395a11d2426af
i-kouya <n_tobira@yahoo.co.jp>
2014-06-14 18:10:27 +0900

    test

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<title>test</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="/prof/www/js/bootstrap.js">
+	<style src="/prof/www/css/bootstrap.css">
+  </head>
+  <body>
+    <h1>test</h1>
+  </body>
+</html>
