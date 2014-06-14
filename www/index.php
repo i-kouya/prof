476dfb4e3bbf7b6e6845c96043bd7715e53c1551
kanaty <yuzukishiiko2@gmail.com>
2014-06-14 19:55:21 +0900

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-	<title>testhogehoge</title>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="/prof/www/js/bootstrap.js">
-	<style src="/prof/www/css/bootstrap.css">
+    <meta charset="utf-8" />
+    <title>testhogehoge</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/prof/www/js/bootstrap.js"></script>
+    <style src="/prof/www/css/bootstrap.css"></style>
   </head>
   <body>
     <h1>test</h1>
     <div class="desc">
-    	testtestkanaty
+    123
+     testtest
+     fugafuga
     </div>
   </body>
 </html>
