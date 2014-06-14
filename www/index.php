58cfd5c2d20f2956fb752281932c19fbf53df300
i-kouya <n_tobira@yahoo.co.jp>
2014-06-14 18:19:56 +0900

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-	<title>test</title>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="/prof/www/js/bootstrap.js">
-	<style src="/prof/www/css/bootstrap.css">
+    <meta charset="utf-8" />
+    <title>test</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="/prof/www/js/bootstrap.js"></script>
+    <style src="/prof/www/css/bootstrap.css"></style>
   </head>
   <body>
     <h1>test</h1>
